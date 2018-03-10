4645c82e1d86475c869f33edbf09724e71ae2e96
jordanpaoletti <jordanspaoletti@gmail.com>
2018-03-10 13:34:28 -0700

    First attempt unsuccessful

--- src/email/Emailer.java
+++ src/email/Emailer.java
@@ -1,5 +1,7 @@
 package email;
 
+import com.sun.mail.smtp.SMTPMessage;
+
 import java.util.*;
 import java.io.*;
 import java.nio.file.*;
@@ -8,19 +10,71 @@ import javax.mail.internet.*;
 public class Emailer {
     private Properties config;
 
-    public static void main(String[] args) {
-        Properties c = new Properties();
-        try (InputStream is = Files.newInputStream(Paths.get("mailconfig"))) {
-            c.load(is);
-            Emailer emailer = new Emailer(c);
+    //gmail account for email
+    private static final String ACCOUNT = "bsucs271emailer@gmail.com";
+    private static final String PASSWORD = "Jtmb)\"Z}@/(6h\"Fj";
 
-            emailer.sendEmail("",
-                    "jordanpaoletti@u.boisestate.edu",
-                    "test",
-                    "test");
-        }
-        catch (IOException e) {
-            System.err.println(e);
+    public static void main(String[] args) {
+//        Properties c = new Properties();
+//        try (InputStream is = Files.newInputStream(Paths.get("mailconfig"))) {
+//            c.load(is);
+//            Emailer emailer = new Emailer(c);
+//
+//            emailer.sendEmail("",
+//                    "jordanpaoletti@u.boisestate.edu",
+//                    "test",
+//                    "test");
+//        }
+//        catch (IOException e) {
+//            System.err.println(e);
+//        }
+
+        String to = "jordanpaoletti@u.boisestate.edu";
+        String from = ACCOUNT;
+
+        System.out.println("Getting properties");
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.socketFactory.port", "465");
+        properties.put("mail.smtp.socketFactory.Class", "javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.user", ACCOUNT);
+        properties.put("mail.password", PASSWORD);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.port", 805);
+        System.out.println("Finished Getting properties");
+
+        System.out.println("Creating Session");
+        Session session = Session.getDefaultInstance(properties, new Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(ACCOUNT, PASSWORD);
+                    }
+                });
+
+        System.out.println("Finished Creating Session");
+
+        try {
+            System.out.println("Creating Message");
+            SMTPMessage message = new SMTPMessage(session);
+
+            System.out.println("Populating message");
+            message.setFrom(new InternetAddress(from));
+            message.setRecipient(Message.RecipientType.TO,
+                    new InternetAddress(to));
+            message.setSubject("Test email from java");
+            message.setText("This is a test email from CS271 project");
+            message.setNotifyOptions(SMTPMessage.NOTIFY_SUCCESS);
+            int returnOpt = message.getReturnOption();
+            System.out.println(returnOpt);
+
+
+            System.out.println("Sending Message");
+            Transport.send(message);
+            System.out.println("Sent message successfully");
+
+        } catch (MessagingException e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
