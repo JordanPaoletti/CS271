a693ec165e8d43b9bd34bfeb8590346f94884190
jordanpaoletti <jordanspaoletti@gmail.com>
2018-03-10 14:10:27 -0700

    Implemented the Email functionality

--- src/email/Emailer.java
+++ src/email/Emailer.java
@@ -1,75 +1,95 @@
 package email;
 
-import com.sun.mail.smtp.SMTPMessage;
-
+import login.UserAccount;
 import java.util.*;
-import java.io.*;
-import java.nio.file.*;
 import javax.mail.*;
 import javax.mail.internet.*;
+
+/**
+ * Sends emails through project gmail account using the sendEmail static method
+ */
 public class Emailer {
-    private Properties config;
+
+    /**
+     * Used to test the class
+     * simply set the local String as follows
+     * to: recipient's email address
+     * sub: subject of the email
+     * body: body of the email
+     */
+    public static void main(String[] args) {
+        String to = null;
+        String sub = null;
+        String body = null;
+
+        sendEmail(to, sub, body);
+    }
+
+    /**
+     *
+     * Sends an email through project gmail account
+     * @param toEmail recipient of the email
+     * @param subject subject line of the email
+     * @param body body of the email
+     * @throws RuntimeException if literally anything goes wrong
+     */
+    public static void sendEmail(String toEmail,
+                          String subject,
+                          String body) {
+        if (toEmail == null || subject == null || body == null) {
+            throw new RuntimeException("Parameters cannot be null!");
+        }
+        else if (!UserAccount.isValidEmail(toEmail)) {
+
+        }
+        Session session = Session.getDefaultInstance(
+                getProperties(),
+                getAuthenticator()
+        );
+
+        try {
+            //create message
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(ACCOUNT));
+            message.setRecipient(Message.RecipientType.TO,
+                    new InternetAddress(toEmail));
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+        } catch (Exception e) {
+            throw new RuntimeException("An error occurred while emailing!", e);
+        }
+
+    }
+
+    //Private
 
     //gmail account for email
     private static final String ACCOUNT = "bsucs271emailer@gmail.com";
     private static final String PASSWORD = "Jtmb)\"Z}@/(6h\"Fj";
 
-    public static void main(String[] args) { //tls Attempt
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+    /**
+     * @return returns the properties for the email session
+     */
+    private static Properties getProperties() {
+        Properties p = new Properties();
+        p.put("mail.smtp.auth", "true");
+        p.put("mail.smtp.starttls.enable", "true");
+        p.put("mail.smtp.host", "smtp.gmail.com");
+        p.put("mail.smtp.port", "587");
+        return p;
+    }
 
-        Session session = Session.getDefaultInstance(props, new Authenticator() {
+    /**
+     * @return an authenticator for the email session
+     */
+    private static Authenticator getAuthenticator() {
+        return new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(ACCOUNT, PASSWORD);
             }
-        });
-
-        try {
-
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(ACCOUNT));
-            message.setRecipient(Message.RecipientType.TO,
-                    new InternetAddress("sammcmahon@u.boisestate.edu"));
-            message.setSubject("Got it ;)");
-            message.setText("The emailer is working!");
-
-            Transport.send(message);
-
-            System.out.println("sent");
-
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-    public Emailer(Properties config) {
-        this.config = config;
-    }
-
-    public void sendEmail(String fromEmail,
-                          String toEmail,
-                          String subject,
-                          String body) {
-        Session session = Session.getDefaultInstance(config, null);
-        MimeMessage message = new MimeMessage(session);
-
-        try {
-            message.addRecipient(
-                    Message.RecipientType.TO,
-                    new InternetAddress(toEmail));
-
-            message.setSubject(subject);
-            message.setText(body);
-            Transport.send(message);
-        }
-        catch (MessagingException e) {
-            System.err.println("Unable to send email:" + e);
-        }
+        };
     }
 }
